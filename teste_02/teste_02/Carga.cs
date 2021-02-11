dd9c64b8d34436736b9a910af5bf191150398954
Gabriel Espinhara Da Silva <gabrielespinhara13@gmail.com>
2021-02-11 16:22:28 -0300

    first commit

---
+++ teste_02/teste_02/Carga.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teste_02
+{
+    public class Carga
+    {
+        public decimal valor { get; set; }
+        public decimal peso { get; set; }
+    }
+}
---
+++ teste_02/teste_02/Transporte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace teste_02
+{
+    class Transporte
+    {
+        public  Transporte(Carga carga)
+        {
+            decimal calculaFrete()
+            {
+                decimal frete;
+                frete = carga.peso* 20/100+ carga.valor *10/100;
+                return frete;
+            }
+        }
+    }
+}
